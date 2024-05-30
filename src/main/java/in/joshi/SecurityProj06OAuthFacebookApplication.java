b4ac9647218dab06843f9b16a33d27a35cbadea0
JoshiNagendra <joshichennamsetti999@gmail.com>
2024-05-30 23:07:43 +0530

    Initial commit

---
+++ src/main/java/in/joshi/SecurityProj06OAuthFacebookApplication.java
@@ -0,0 +1,13 @@
+package in.joshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityProj06OAuthFacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityProj06OAuthFacebookApplication.class, args);
+	}
+
+}
